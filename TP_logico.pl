1085c9ce7feec8870d8b184a9d88dbbb6df59248
Yane801 <37596259+Yane801@users.noreply.github.com>
2018-07-13 15:42:50 -0300

    cambios

--- TP_logico.pl
+++ TP_logico.pl
@@ -18,12 +18,13 @@ incomodidad(notequieromas,10).
 
 %variblesenmayus
 %1
-sonAmigos(P1,P2):- contacto(P1,P2),contacto(P2,P1),mensaje(P1,P2,_), P1\=P2 .
-sonAmigos(P1,P2):- contacto(P1,P2),contacto(P2,P1),mensaje(P2,P1,_), P1\=P2 .
+sonAmigos(P1,P2):- contacto(P1,P2),contacto(P2,P1),P1\=P2, seMandaronUnMensajeEntreEllos(P1,P2).
+
+seMandaronUnMensajeEntreEllos(P1,P2):- mensaje(P1,P2,_).    
+seMandaronUnMensajeEntreEllos(P1,P2):- mensaje(P2,P1,_).
 
 %2
-recMsjInesperado(Emisor,P1) :- not(contacto(P1,Emisor)),
-                                mensaje(Emisor,P1,_), 
-                                   Emisor\=P1 .
+recMsjInesperado(Emisor,P1) :-  mensaje(Emisor,P1,_),
+                                not(contacto(P1,Emisor)).
 %3
 recvMsjIncomodo(P2) :- mensaje(_,P2,Msj), incomodidad(Msj,Inc), Inc>7 .							   
